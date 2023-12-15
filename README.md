3a1c80a56a23bea7940e12b2daaa4445381bdeee
Fmella <francisco.jmella.r@gmail.com>
2023-12-14 21:52:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-fwbs9t
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Fmella/stackblitz-starters-fwbs9t)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './style.css';
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://mockly.app/api/38534cce-0c18-4043-94a6-21b0ae62f48a/speed')
+      .then((response) => response.json())
+      .then((newData) => {
+        setData(newData[0]);
+      });
+  });
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
